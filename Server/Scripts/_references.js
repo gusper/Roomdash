8a3e3ca62813b1630baf03cca80f55f24367c44b
Gus Perez <gus@gusperez.com>
2016-10-06 08:56:25 -0700

    Updated to latest bcl targets and jquery

--- Server/Scripts/_references.js
+++ Server/Scripts/_references.js
@@ -1,5 +1,5 @@
-﻿/// <reference path="jquery-2.1.0.js" />
-/// <reference path="jquery-ui-1.10.4.js" />
+﻿/// <reference path="jquery-ui-1.12.1.js" />
+/// <reference path="jquery-3.1.1.js" />
 /// <reference path="modernizr-2.7.2.js" />
 /// <reference path="libs\jquery.validate.js" />
 /// <reference path="libs\jquery.validate.unobtrusive.js" />
